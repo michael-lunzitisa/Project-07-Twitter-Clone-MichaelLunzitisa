3e01bab7984c13b362cbb599f74e88411ea72490
Michael <mickaellunzola@gmail.com>
2023-12-01 07:54:38 +0100

    fix: a bug

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,7 +1,7 @@
 import Header from "./Header";
 import TweetEditor from "./TweetEditor";
 import Tweets from "./tweets";
-//gggggggggg
+
 const Timeline = () => {
     return (
         <div className="timeline">
