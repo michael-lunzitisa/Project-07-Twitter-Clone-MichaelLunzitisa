1565e5271e768e2d5c17ed62f0ce9ed236ac7e55
Michael Lunzola <Michael>
2024-01-01 13:11:14 +0100

    fix: a bug

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,17 +1,17 @@
 import ProfileTitle from "./profileTitle";
 import UserImage from "./userImage";
 import UserAbout from "./userAbout";
-import ProfileNav from "./ProfileNav";
+import ProfileNav from "./profileNav";
 
 const Profile = () => {
-    return (
-        <div className="timeline">
-            <ProfileTitle />
-            <UserImage />
-            <UserAbout />
-            <ProfileNav />
-        </div>
-    );
+  return (
+    <div className="timeline">
+      <ProfileTitle />
+      <UserImage />
+      <UserAbout />
+      <ProfileNav />
+    </div>
+  );
 };
 
 export default Profile;
