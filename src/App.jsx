6ae4cc5e69e785e8c40f301ce16b4fb18f6088c1
Michael <mickaellunzola@gmail.com>
2024-01-19 09:07:47 +0100

    fix: a bug in component avatar

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,17 @@ import Trends from "./components/trends/trends.jsx";
 import Profile from "./pages/profile.jsx";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Sidebar />
+    return (
+        <BrowserRouter>
+            <Sidebar />
 
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-        <Trends />
-      </Layout>
-    </BrowserRouter>
-  );
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/:author" element={<Profile />} />
+                </Routes>
+                <Trends />
+            </Layout>
+        </BrowserRouter>
+    );
 }
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -4,7 +4,11 @@ import UserAbpout from "../components/profile/userAbout";
 import ProfileNav from "../components/profile/profileNav";
 import UserDetails from "../components/profile/UserDetails";
 
+import { useParams } from "react";
+
 const Profile = () => {
+    // const { id } = useParams();
+    // const user = data.users.find((user) => user.id === id);
     return (
         <div className="timeline">
             <PageTitle />
